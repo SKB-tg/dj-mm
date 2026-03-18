1316f359e3613850fd558d1bf426f090f85bb442
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2026-03-18 22:51:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from './express';
-import cors from '../cors';
-import dotenv from './dotenv';
-import pool from './db.js';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pool from 'server/db.js';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 dotenv.config();
