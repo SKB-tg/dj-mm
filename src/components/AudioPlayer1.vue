25f3677a9fdc13f0a5de8b1e8c6563530217d426
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2026-06-01 11:12:44 +0300

    Update AudioPlayer1.vue

--- src/components/AudioPlayer1.vue
+++ src/components/AudioPlayer1.vue
@@ -182,7 +182,7 @@ function playNext(playIndex) {
 
   audio.src = currentTrack.value.url
   audio.crossOrigin = "anonymous";
-	progressPercent.value == 0
+	audioRef.progressPercent == 0
   audio.pause()
   isPlaying.value = false
        emit('track-change', audioRef, currentTrack.value, idplaylist.value)
@@ -203,7 +203,7 @@ function playNext(playIndex) {
   const audio = audioRef.value
   audio.src = currentTrack.value.url
   audio.crossOrigin = "anonymous";
-	progressPercent.value == 0
+	audioRef.progressPercent == 0
   audio.pause()
   isPlaying.value = false
        emit('track-change', audioRef, currentTrack.value, idplaylist.value)
