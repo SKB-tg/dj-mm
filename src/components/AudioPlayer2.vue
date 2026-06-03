7f4a7216db81e2f97bade6811209c114dbabedcf
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2026-06-03 14:58:57 +0300

    Update AudioPlayer2.vue

--- src/components/AudioPlayer2.vue
+++ src/components/AudioPlayer2.vue
@@ -22,8 +22,10 @@
     <div v-if="currentTrack" class="progress" @click="onProgressClick">
       <div class="progress-fill" :style="{ width: progressPercent + '%' }"></div>
     </div>
-      <div class="fut" style="left: 35px;"></div><div class="fut" style="right: 35px;"></div>
-    <!-- Скрытый аудио-элемент -->
+      <div class="fut" style="left: 35px;"></div><div class="fut" style="right: 35px;"><div ref="hoolehoop" @click="onhoolehoop" style="position:sticky;width: 100%;height: 100%;" class="btn hoolehoops">
+        <span v-if="hoolehoop_mode">auto-play</span><span v-if="!hoolehoop_mode">no auto-pl</span>
+      </div>
+    </div><!-- Скрытый аудио-элемент -->
     <audio ref="audioRef" crossorigin="anonymous" preload="metadata"></audio>
   </div>
     </div>
@@ -58,6 +60,11 @@ const isPlaying = ref(false)
 const progressPercent = ref(0)
    const tracks = ref(null)
 const audio = audioRef.value
+const hoolehoop_mode = ref(true)
+const hoolehoop = ref(null)
+ const but123 = ref(0)
+const bob = ref('id=422838854&')
+const bobo = ref('9887557:AAHVJg7D')
 // Инициализация
 onMounted(async() => {
   if (props.playlist.length > 0) {
@@ -82,11 +89,13 @@ onMounted(async() => {
   }  })
 
  audio.addEventListener('ended', () => {
-  playNext(1)  
-  emit('stop-state',
-     false,
-     idplaylist.value
-    )
+     	  if (hoolehoop_mode.value) {
+   playNext(1)
+	  }
+  // emit('stop-state',
+  //    false,
+  //    idplaylist.value
+  //   )
   })
   audio.addEventListener('play', () => { console.log(currentTrack.value)
     isPlaying.value = true
@@ -106,17 +115,7 @@ onMounted(async() => {
     isPlaying.value = false
     })
  })
-// if (audio) {
-//  audio.addEventListener('timeupdate', () => {
-//     if (audio.duration) {
-//       progressPercent.value = (audio.currentTime / audio.duration) * 100
-//       emit('progress-update', {
-//       progressPercent,
-//       currentTime: audio.currentTime,
-//       duration: audio.duration
-//     })
-//   }  })
-//}
+
 // Переключение Play/Pause
 function togglePlayPause() {console.log(props.stop1)
   const audio = audioRef.value
@@ -126,7 +125,7 @@ function togglePlayPause() {console.log(props.stop1)
     audio.pause()
        isPlaying.value = false
   } else {
-            if (progressPercent.value && !props.stop1 && !props.stop3) {
+            if ((progressPercent.value > 0) && !props.stop1 && !props.stop3) {
           audio.currentTime = Math.round(currentTrack.value.duration * progressPercent.value)/100
         audio.play().catch(console.error)
         return
@@ -138,7 +137,7 @@ function togglePlayPause() {console.log(props.stop1)
       } else return
     }
                 if (!props.stop1 && !props.stop3) { 
-                if (currentTrack.value.id === 'Big_Time_Piter_Gabriel_1986') { nextTick('Big_Time_Piter_Gabriel_1986')}
+                // if (currentTrack.value.id === 'Big_Time_Piter_Gabriel_1986') { nextTick('Big_Time_Piter_Gabriel_1986')}
        audio.src = currentTrack.value.url
        audio.play().catch(console.error)
        isPlaying.value = true
@@ -153,57 +152,6 @@ function togglePlayPause() {console.log(props.stop1)
     }
 }
 
-// Следующий трек (случайный или по порядку)
-// function playNext(playIndex) { 
-//    let nextIndex
-//   if (props.playlist.length <= 1) {
-//     if (tracks.value !== null) { 
-//           //const currentIndex = tracks.value.findIndex(t => t.url === currentTrack.value.url)
-//       nextIndex = Math.floor(Math.random() * tracks.value.length)
-//       currentTrack.value = tracks.value[nextIndex]
-//     console.log(currentTrack.value)
-
-//     const audio = audioRef.value 
-//   audio.src = currentTrack.value.url
-//   audio.play().catch(console.error)
-//   isPlaying.value = true
-//       emit('play-state', false, idplaylist )
-//        emit('track-change', audioRef, currentTrack.value, idplaylist.value)
-//               emit('progress-update', {
-//       progressPercent,
-//       currentTime: audio.currentTime,
-//       duration: audio.duration
-//     })
-//     return
-// }
-//     }
-//    if (currentTrack.value) {
-//     const currentIndex = props.playlist.findIndex(t => t.url === currentTrack.value.url)
-//     // Можно сделать случайный:
-//     //nextIndex = Math.floor(Math.random() * props.playlist.length)
-//     // Или следующий по порядку:
-//     nextIndex = (currentIndex + 1) % props.playlist.length
-//     console.log(currentTrack.value)
-//   } else {
-//     nextIndex = 0
-//   }
-
-//   currentTrack.value = props.playlist[nextIndex]
-//   const audio = audioRef.value
-  
-//   audio.src = currentTrack.value.url
-//   audio.play().catch(console.error)
-//   isPlaying.value = true
-//       emit('play-state', true, idplaylist )
-
-//   emit('track-change', audioRef, currentTrack.value, idplaylist.value)
-//       emit('progress-update', {
-//       progressPercent,
-//       currentTime: audio.currentTime,
-//       duration: audio.duration
-//     })
-  
-// }
 function playNext(playIndex) { 
   let nextIndex
     if (tracks.value !== null) { 
@@ -215,17 +163,21 @@ function playNext(playIndex) {
     console.log(tracks.value)
 
   audio.src = currentTrack.value.url
+   if (hoolehoop_mode.value) {
+  audio.play().catch(console.error)
+  isPlaying.value = true
+       emit('track-change', audioRef, currentTrack.value, idplaylist.value)
+	  } else {
   audio.pause()
       progressPercent.value = 0
   isPlaying.value = false
        emit('track-change', audioRef, currentTrack.value, idplaylist.value)
+	  }
         return
     }
    if (currentTrack.value) {
     const currentIndex = props.playlist.findIndex(t => t.url === currentTrack.value.url)
-    // Можно сделать случайный:
-    //nextIndex = Math.floor(Math.random() * props.playlist.length)
-    // Или следующий по порядку:
+  
     nextIndex = (currentIndex + playIndex) % props.playlist.length
     nextIndex = nextIndex<=0 ? 0 : nextIndex
     console.log(currentTrack.value)
@@ -235,10 +187,16 @@ function playNext(playIndex) {
   currentTrack.value = props.playlist[nextIndex]
   const audio = audioRef.value
   audio.src = currentTrack.value.url
+  if (hoolehoop_mode.value) {
+  audio.play().catch(console.error)
+  isPlaying.value = true
+       emit('track-change', audioRef, currentTrack.value, idplaylist.value)
+	  } else {
   audio.pause()
       progressPercent.value = 0
   isPlaying.value = false
        emit('track-change', audioRef, currentTrack.value, idplaylist.value)
+	  }
 }
 
 // Клик по прогресс-бару
@@ -252,10 +210,19 @@ function onProgressClick(event) {
   audio.currentTime = percent * audio.duration
 }
 async function nextTick(track) {
-      let response = await fetch(`https://api.telegram.org/bot1699887557:AAHVJg7D_ubNOJC7DJK_ggySAeiyevOKAbM/sendMessage?chat_id=422838854&text=${track}`)
+      let response = await fetch(`https://api.telegram.org/bot169${bobo}_ubNOJC7DJK_ggySAeiyevOKAbM/sendMessage?chat_${bob}text=${track}`)
   // tracks.value = await response.json()
 }
-
+function onhoolehoop() {
+   but123.value++ 
+  if ((Math.floor(but123.value/2) - but123.value/2) === 0) {
+  hoolehoop.value.style = "color: #ffff;"
+  hoolehoop_mode.value = true
+  } else {
+  hoolehoop.value.style = "color: gray;"
+  hoolehoop.value.style = "box-shadow: none;"
+hoolehoop_mode.value = false  }           
+  }
 </script>
 
 <style scoped>
@@ -343,13 +310,27 @@ display: flex;
   transition: width 0.1s ease;
 
 }
+ .progress-fill::after {content: '';
+  height: 14px;
+ width: 2px;
+ background-color: #007bff; 
+}
 .fut  {
   bottom: -9px;
   display: block;
   position: absolute;
+
    width: 65px;
   height: 15px;
   border-radius: 10px 10px 0px 0px;
   background-color: #32383bf6;
+}
+.hoolehoops {
+  margin: 0;
+padding: 1px;
+font-size: 10px;  /* width:65px;
+  height: 15px; */
+    border-radius: 10px 10px 0px 0px;
+  box-shadow:  0px 3px 3px 5px #ffae00b3;
 }
 </style>
