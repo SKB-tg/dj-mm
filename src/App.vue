c709ddc6f474cc042e74435a741f58f64f1005a2
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2026-07-16 23:48:51 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -73,7 +73,7 @@ import AudioPlayer2 from './components/AudioPlayer2.vue'
 import AudioPlayer3 from './components/AudioPlayer3.vue'
 import FloatingModal from './components/FloatingModal.vue'
 
-const showModal = ref(true)
+const showModal = ref(false)
 
 // Эти данные могут приходить из tracks.json или API
 const Spaceelectro = ref([])
